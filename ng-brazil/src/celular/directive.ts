261319628c47f15cdc9dce50477e8446621fe4cb
woj1965 <23257632+woj1965@users.noreply.github.com>
2018-09-21 18:05:17 -0300

    Create directive.ts
    celular validation

---
+++ ng-brazil/src/celular/directive.ts
@@ -0,0 +1,20 @@
+import { Directive, forwardRef } from '@angular/core';
+import { NG_VALIDATORS, Validator, AbstractControl } from '@angular/forms';
+import { celular } from './validator';
+
+const CELULAR_VALIDATOR: any = {
+  provide: NG_VALIDATORS,
+  useExisting: forwardRef(() => CelularValidator),
+  multi: true
+};
+
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: '[celular][formControlName],[celular][formControl],[celular][ngModel]',
+  providers: [CELULAR_VALIDATOR]
+})
+export class CelularValidator implements Validator {
+  validate(c: AbstractControl): {[key: string]: any} {
+    return celular(c);
+  }
+}
