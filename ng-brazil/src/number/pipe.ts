a90f72c374733e6fbbf9c6ca12a86a27b2fbd05a
Mario Mol <mariohmol@gmail.com>
2019-09-18 19:30:41 -0300

    #32

--- ng-brazil/src/number/pipe.ts
+++ ng-brazil/src/number/pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { maskBr } from 'js-brasil';
 
 @Pipe({
-  name: 'number',
+  name: 'numberBrazil',
 })
 export class NUMBERPipe implements PipeTransform {
   transform(numberValue: any) {
